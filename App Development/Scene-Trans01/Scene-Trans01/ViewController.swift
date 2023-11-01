1e24c125d28b99b0c0b613b388444b50b93d1121
ShinIlHeo <shintwl@icloud.com>
2023-11-01 11:04:10 +0900

    created Scene Transition First App

---
+++ App Development/Scene-Trans01/Scene-Trans01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-Trans01
+//
+//  Created by siheo on 11/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
