18ff1d6d3450ee4bc9f51d5028cca5e10667fda5
ShinIlHeo <shintwl@icloud.com>
2023-11-01 11:21:39 +0900

    created Scene-Trans02 App
    This app is for practicing switching between view controllers using UINavigationViewController

---
+++ App Development/Scene-Trans02/Scene-Trans02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-Trans02
+//
+//  Created by siheo on 11/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
