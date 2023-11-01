3a3c38730c12950199da32ed0bede01b05c22339
ShinIlHeo <shintwl@icloud.com>
2023-11-01 10:34:49 +0900

    implemented IBTest App functionalities
    practiced how to connect components on storyboard with UIViewController

--- App Development/IBTest/IBTest/ViewController.swift
+++ App Development/IBTest/IBTest/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var uiTitle01: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var uiTitle02: UILabel!
+
+    @IBOutlet weak var uiTitle03: UILabel!
+
+    @IBOutlet weak var uiTitle04: UILabel!
+
+    @IBOutlet weak var uiTitle05: UILabel!
+    
+    @IBAction func clickBtn01(_ sender: Any) {
+        self.uiTitle01.text = "Button01 clicked"
     }
 
+    @IBAction func clickBtn02(_ sender: Any) {
+        self.uiTitle02.text = "Button02 clicked"
+    }
 
+    @IBAction func clickBtn03(_ sender: Any) {
+        self.uiTitle03.text = "Button03 clicked"
+    }
+
+    @IBAction func clickBtn04(_ sender: Any) {
+        self.uiTitle04.text = "Button04 clicked"
+    }
+
+    @IBAction func clickBtn05(_ sender: Any) {
+        self.uiTitle05.text = "Button05 clicked"
+    }
 }
 
