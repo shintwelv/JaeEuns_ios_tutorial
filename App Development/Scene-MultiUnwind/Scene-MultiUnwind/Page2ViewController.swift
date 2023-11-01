4f51cd392c29c9e8fdb9b141f5c495a3372bb885
ShinIlHeo <shintwl@icloud.com>
2023-11-01 12:16:22 +0900

    implemented Scene-MultiUnwind functionalities
    - present specific view controller using unwind

---
+++ App Development/Scene-MultiUnwind/Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by siheo on 11/1/23.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+    
+    @IBAction func gotoPage2(_ segue:UIStoryboardSegue) {
+        
+    }
+    
+}
