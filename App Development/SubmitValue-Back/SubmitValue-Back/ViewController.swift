421595c289ce84126361450f82004224240e7c28
ShinIlHeo <shintwl@icloud.com>
2023-11-02 10:01:55 +0900

    created SubmitValue-Back App
    this app is for practicing passing data to previous view controller

---
+++ App Development/SubmitValue-Back/SubmitValue-Back/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmitValue-Back
+//
+//  Created by siheo on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
