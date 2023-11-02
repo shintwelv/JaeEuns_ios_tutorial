77497e615b34acab7f7cb36ca203f27f72dacf11
elvinHeo <shintwl@icloud.com>
2023-11-02 19:11:52 +0900

    created and implemented Local Push - from iOS 13, add notification request should be done on SceneDelegate

---
+++ App Development/Msg-Notification/Msg-Notification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Msg-Notification
+//
+//  Created by ShinIl Heo on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
