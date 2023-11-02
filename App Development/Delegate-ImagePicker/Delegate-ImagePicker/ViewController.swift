3935f5d1b0add7ac8595e83acb019e0ca4e5385b
elvinHeo <shintwl@icloud.com>
2023-11-02 21:04:44 +0900

    implemented UIImagePicker and its delegate methods

--- App Development/Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
+++ App Development/Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imgView: UIImageView!
+
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary // 이미지 소스로 사진 라이브러리 선택
+        picker.allowsEditing = true // 이미지 편집 기능 사용
+        
+        picker.delegate = self
+        
+        self.present(picker, animated: false)
     }
-
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // 이미지 피커 컨트롤러 창 닫기
+        picker.dismiss(animated: false) {
+            let alert = UIAlertController(title: "",
+                                          message: "이미지 선택이 취소되었습니다", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: false)
+        }
+    }
+    
+    // 이미지 피커에서 이미지를 선택했을 때 호출되는 메소드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: false) {
+            let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            self.imgView.image = img
+        }
+    }
+    
 }
 
