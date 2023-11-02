7fbcbd8cbb0b49e9f31581566436bf331b864e55
elvinHeo <shintwl@icloud.com>
2023-11-02 20:43:42 +0900

    created Delegate-ImagePicker App - this app is for practicing delegate pattern and UIImagePicker functionalities

---
+++ App Development/Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate-ImagePicker
+//
+//  Created by ShinIl Heo on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
