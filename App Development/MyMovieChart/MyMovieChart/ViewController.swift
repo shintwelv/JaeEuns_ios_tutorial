cabfab650268aca2100402c425508f02764d9161
siheo <shintwl@icloud.com>
2023-11-03 15:17:47 +0900

    created MyMovieChart App
    this app was created for practicing configuring UITableViewController

---
+++ App Development/MyMovieChart/MyMovieChart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by siheo on 11/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
