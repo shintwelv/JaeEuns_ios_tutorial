b91e4f3e1bb54607547de625b4c3205fed63561f
elvinHeo <shintwl@icloud.com>
2023-11-03 20:54:11 +0900

    implemented MyMovieChart-SpinOff functionalities - created UITableView on UIViewController - manually set and adopted delegate and datasource

--- App Development/MyMovieChart-SpinOff/MyMovieChart-SpinOff/ViewController.swift
+++ App Development/MyMovieChart-SpinOff/MyMovieChart-SpinOff/ViewController.swift
@@ -17,3 +17,21 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 //임의로 5개 셀 생성
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)번째 데이터 입니다"
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("\(indexPath.row)번째 데이터가 클릭됨")
+    }
+}
+
